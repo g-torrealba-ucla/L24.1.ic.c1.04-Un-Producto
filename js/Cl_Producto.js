5be75a7d560874a5e1eb91850f7c1f0f6df7be01
g_torrealba <g_torrealba@ucla.edu.ve>
2024-07-03 13:05:32 +0000

    240703-0905-G) Tst Project IDX

--- js/Cl_Producto.js
+++ js/Cl_Producto.js
@@ -1,46 +1,40 @@
-export default class Cl_Producto{
-    constructor (codigo,costo,tipo){
-        this.codigo = codigo;
-        this.costo = costo;
-        this.tipo = tipo;
-    }
+export default class Cl_Producto {
+  constructor(codigo, costo, tipo) {
+    this.codigo = codigo;
+    this.costo = costo;
+    this.tipo = tipo;
+  }
+  set codigo(codigo) {
+    this._codigo = codigo;
+  }
+  get codigo() {
+    return this._codigo;
+  }
+  set costo(costo) {
+    this._costo = +costo;
+  }
+  get costo() {
+    return this._costo;
+  }
+  set tipo(tipo) {
+    this._tipo = +tipo;
+  }
+  get tipo() {
+    return this._tipo;
+  }
 
-    set codigo( c){
-      this._codigo = c;
-    }
+  precioBaseVenta() {
+    return (this.costo * 0.30) + this.costo;
+  }
+  descuento() {
+    if (this.tipo == 3)
+      return this.precioBaseVenta() * 0.10;
+    else
+      return 0;
+  }
 
-    get codigo(){
-      return this._codigo;
-    }
-
-    set costo(ct){
-      this._costo = +ct;
-    }
-     
-    get costo (){
-      return this._costo;   
-    }
-
-    set tipo(t){
-      this._tipo = +t;
-    }   
-
-    get tipo (){
-      return this._tipo;   
-    }       
-
-precioBaseVenta(){
-     return (this.costo *0.30) + this.costo;
-}
-    descuento(){
-        if (this.tipo == 3) 
-             return this.precioBaseVenta()* 0.10;
-            else
-             return 0;
-    }
-
-    precioFinal(){  
-        return this.precioBaseVenta() - this.descuento();
-    }
+  precioFinal() {
+    return this.precioBaseVenta() - this.descuento();
+  }
 
 }
